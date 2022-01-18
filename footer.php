86db185967ec6003865ca1922858eb44aff1d6a4
Gerald Tan Yuan Xin (ZNX) <gerald.tan@zionext.com>
2022-01-18 19:25:13 +0800

    Setup Files and Folders

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div> <!-- close the column in 3rd Row -->
+            </div> <!-- close the 3rd Row -->
+            <!-- 4th Row -->
+            <div class="row" >
+                <div class="col-sm-12" style="text-align: right; ">
+                    <hr/>
+                    Do you need help? Please email to:
+                    <a href="mailto:mr.donut@business.sg">Donut@business.sg</a>
+                    <p style="font-size: 12px" >&copy;Copyright by Donut Group</p>
+                </div>
+            </div>
+        </div>
+        <!--  Close the bootstrap container -->
+    </body> 
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Mr. Donut</title>
+        <!-- jQuery Library -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Latest compiled JavaScript -->
+        <script src="js/jsquery-3.3.1.min.js"></script>
+        <!-- Site specific Cascading Stylesheet -->
+        <link rel="stylesheet" href="css/site.css">
+        <script type= "text/javascript" defer="defer" src="js/script.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <!-- 1st Row -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <a href="index.php">
+                        <img src="Images/mrdonut.jpg" alt="Logo"
+                        class="img-fluid" style="width:100%"/></a>
+                </div>
+            </div>
+            <!-- 2nd Row -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <?php include ("navbar.php") ; ?>
+                </div>
+            </div>
+            <!-- 3rd Row -->
+            <div class="row">
+                <div class="col-sm-12" style="padding: 15px;">
+                    <!-- define customised content here -->
+                    <!-- closing of this column and row is in footer. php -->
+                
