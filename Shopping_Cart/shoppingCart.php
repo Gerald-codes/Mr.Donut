7f46d8f8e706aee7dd3620d900be524b0a8c6512
Gerald <Gerald031313@gmail.com>
2022-02-05 18:48:15 +0800

    Fix navigation issue

--- Shopping_Cart/shoppingCart.php
+++ Shopping_Cart/shoppingCart.php
@@ -1,13 +1,14 @@
 <?php 
+// Include the code that contains shopping cart's functions.
+// Current session is detected in cartFunctions.php, hence need not start session here.
+include_once("cartFunctions.php");
+
 if (! isset($_SESSION["ShopperID"])) { // Check if user logged in 
 	// redirect to login page if the session variable shopperid is not set
 	header ("Location: ../login.php");
 	exit;
 }
 
-// Include the code that contains shopping cart's functions.
-// Current session is detected in cartFunctions.php, hence need not start session here.
-include_once("cartFunctions.php");
 include("../header.php"); // Include the Page Layout header
 
 
