0bdfb620dd5bc403a90752432cb7bfa55a0133aa
s10177125 <s10177125@connect.np.edu.sg>
2022-02-06 22:11:15 +0800

    Changed CSS for table and button, added comments

--- Shopping_Cart/shoppingCart.php
+++ Shopping_Cart/shoppingCart.php
@@ -11,7 +11,6 @@ if (! isset($_SESSION["ShopperID"])) { // Check if user logged in
 
 include("../header.php"); // Include the Page Layout header
 
-
 echo "<div id='myShopCart' style='margin:auto'>"; // Start a container
 if (isset($_SESSION["Cart"])) {
 	include_once("../Database/mysql_conn.php");
@@ -24,22 +23,22 @@ if (isset($_SESSION["Cart"])) {
 	$result = $stmt->get_result();
 	$stmt->close();
 	
+	// Create product details table
 	if ($result->num_rows > 0) {
 		echo "<p class='page-title' style='text-align:center'>Shopping Cart</p>"; 
 		echo "<div class='table-responsive' >"; // Bootstrap responsive table
 		echo "<table class='table table-hover'>"; // Start of table
 		echo "<thead class='cart-header'>";
 		echo "<tr>";
-		echo "<th width='250px'>Item</th>"; 
-		echo "<th width='90px'>Price (S$)</th>";
-		echo "<th width='60px'>Quantity</th>";
-		echo "<th width='120px'>Total (S$)</th>";
-		echo "<th>&nbsp;</th>";
+		echo "<th class='header' width='250px'>Item</th>"; 
+		echo "<th class='header' width='90px'>Price (S$)</th>";
+		echo "<th class='header' width='60px'>Quantity</th>";
+		echo "<th class='header' width='120px'>Total (S$)</th>";
+		echo "<th class='header' >&nbsp;</th>";
 		echo "</tr>"; // End of table row
 		echo "</thead>"; // End of table's header section
 
 		$_SESSION["Items"] = array();	
-
 
 		$subTotal = 0; // Declare a variable to compute subtotal before tax
 		echo "<tbody>"; // Start of table's body section
@@ -59,6 +58,7 @@ if (isset($_SESSION["Cart"])) {
 					$selected = "";
 				echo "<option value='$i' $selected>$i</option>";
 			}
+			// Inputting shopping cart items into table
 			echo "</select>";
 			echo "<input type='hidden' name='action' value='update' />";
 			echo "<input type='hidden' name='product_id' value='$row[ProductID]' />";
@@ -87,42 +87,27 @@ if (isset($_SESSION["Cart"])) {
 		echo "</tbody>"; // End of table's body section
 		echo "</table>"; // End of table
 		echo "</div>"; // End of Bootstrap responsive table
-    
+		
+		// Counting total number of items
 		$totalQuantity = 0;
 		foreach ($_SESSION["Items"] as $purchased) {
 			$totalQuantity += $purchased['quantity'];
 		}
-		echo "<p style='text-align:right; font-size:20px'>
-		Subtotal = S$". number_format($subTotal, 2), 
+
+		// Printing out subtotal and total number of items
+		echo "<p style='text-align:center; font-size:20px'>
+		Subtotal: S$". number_format($subTotal, 2), 
 		"</br>Total quantity of items: $totalQuantity"; // Additional 2. Compute number of items in cart
   		$_SESSION["SubTotal"] = round($subTotal, 2);	
+		
+		// Printing out notice for waived delivery charge
+		if ($subTotal > 50) {
+			echo "</br>";
+			echo "As your subtotal is more than $50, you are eligible for $0 delivery fee when you choose
+			Normal Delivery!</p>";
+		}
 
-		// Add PayPal Checkout button on the shopping cart page
-		echo("<button class='open-button' onclick='openForm()'>checkout</button>");
-		echo('<div class="form-popup" id="deliveryForm">');
-		echo('<form method="post" action="../Checkout/checkoutProcess.php" class="form-container">');
-		echo('<h2 id="form-header">Delivery Option</h2>');
-		echo("<div class='options'>");
-		echo('<input type="radio" id="Normal" name="ShipCharge" value="2">');
-		echo('<label for="normal"><h5>Normal Delivery</h5><p>$2</p></label>');
-		echo("</div>");
-		echo("<p> delivered within 1 working day after an order is placed</p>");
-		echo("<div class='options'>");
-		echo('<input type="radio" id="express" name="ShipCharge" value="5">');
-		echo('<label for="express"><h5>Express Delivery</h5><p>$5</p></label>');
-		echo("</div>");
-		echo("<p> delivered within 2 hours after an order is placed</p>");
-		echo("<div class='buttonContainer'>");
-		echo "<input id='formButton' type='image' style='float:right;' onclick='getDeliveryMode()'
-					src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png' 
-					alt='Buy now with PayPal'>";
-		echo('<button type="button" class="closebtn" onclick="closeForm()">Close</button>');
-		echo('</div>');
-		echo('</div>');
-		echo('</form>');
-		echo('<a href="../Checkout/checkoutPage.php"> <input type="button" value="Checkoutnow" /> </a>');
-
-		// Retrieve ShipCharge
+		// Retrieve delivery fee from ShopCart
 		$qry2 = "SELECT ShipCharge FROM ShopCart WHERE ShopCartID=?";
 		$stmt = $conn->prepare($qry2);
 		$stmt->bind_param("i", $_SESSION["Cart"]);
@@ -138,7 +123,7 @@ if (isset($_SESSION["Cart"])) {
 				$_SESSION['ShipCharge'] = 0;
 				$sql = "UPDATE ShopCart SET ShipCharge = ? WHERE ShopCartId = ?  ";
                 $stmt2 = $conn->prepare($sql);
-                $stmt2->bind_param("di",$_SESSION['ShipCharge'], $_SESSION["Cart"]);
+                $stmt2->bind_param("ii",$_SESSION['ShipCharge'], $_SESSION["Cart"]);
                 $stmt2->execute();
                 $stmt2->close();
 			}
@@ -153,6 +138,67 @@ else {
 	echo "<h3 style='text-align:center; color:red;'>Empty shopping cart!</h3>";
 }
 
+echo "</br>";
+echo('<center><a href="../Checkout/checkoutPage.php"> <input type="button" class="check" value="Check out now!" /> </a></center>');
+
 echo "</div>"; // End of container
 include("../footer.php"); // Include the Page Layout footer
 ?>
+
+<!-- Styling for checkout button and table -->
+<style>
+	.check {
+		align-items: center;
+		background-color: #fff;
+		border: 2px solid #000;
+		box-sizing: border-box;
+		color: #000;
+		cursor: pointer;
+		display: inline-flex;
+		fill: #000;
+		font-family: Inter,sans-serif;
+		font-size: 16px;
+		font-weight: 600;
+		height: 48px;
+		justify-content: center;
+		letter-spacing: -.8px;
+		line-height: 24px;
+		min-width: 140px;
+		outline: 0;
+		padding: 0 17px;
+		text-align: center;
+		text-decoration: none;
+		transition: all .3s;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+		}
+
+		.check:focus {
+		color: #171e29;
+		}
+
+		.check:hover {
+		border-color: #06f;
+		color: #06f;
+		fill: #06f;
+		}
+
+		.check:active {
+		border-color: #06f;
+		color: #06f;
+		fill: #06f;
+		}
+
+		@media (min-width: 768px) {
+		.check {
+			min-width: 170px;
+		}
+		}
+
+	.header {
+		color: black;
+		background-color: white;
+	}
+
+</style>
