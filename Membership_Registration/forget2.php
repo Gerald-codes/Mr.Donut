b7ff205068e382a324e802ae0d6761162ba6c784
faris122 <fi2955962@gmail.com>
2022-02-06 16:21:14 +0800

    updates

--- Membership_Registration/forget2.php
+++ Membership_Registration/forget2.php
@@ -22,29 +22,13 @@ if ($result->num_rows > 0) {
     $stmt = $conn->prepare($qry);
     $stmt->bind_param("si", $hashed_pwd, $shopperId);
     $stmt->execute();
-    include("myMail.php");
-    $to=$row['Email'];
-    $from="donut12733889@gmail.com";
-    $from_name="Mr Donut";
-    $subject="Mr Donut Login Password";
-    $body="<span style='color:black; font-size:l2px'> 
-    Your new password is <span style='font-weight:bold'> 
-    $new_pwd</span>.<br /> 
-    Do change this default password.</span>";
-    if(smtpmailer($to, $from, $from_name, $subject, $body)) {
-        echo "<p>Your new password is sent to: 
-        <span style='font-weight:bold'>$to</span>.</p>"; 
-    } 
-    else { 
-        echo "<p><span style='color:red;'> Mailer Error: " . $error . "</span></p>"; 
-        }
+    echo"<span class='page-title'>Password has been reset</span>";
+    echo"<p>Your new password is <b>$new_pwd</b>, please update ASAP for security reasons</p>";
 }
 else {
-    echo "<p><span style='color:red;'>
-        Wrong Answer!</span></p>";
+    echo "<span class='page-title' style='color:red;'>
+        Wrong Answer!</span>";
 }
-
-$stmt->close();
 $conn->close();
 include("../footer.php")
 ?>
--- Membership_Registration/forgetpassword.php
+++ Membership_Registration/forgetpassword.php
@@ -4,19 +4,19 @@ include("../header.php"); // Include the Page Layout header
 include_once("../Database/mysql_conn.php");
 ?>
 <div class="container" style="width:80%; margin:auto;">
-<h2 style="text-align:center">Forget Password</h2>
+<span class="page-title">Password Reset</span>
 <form method="post" >
 	<div class="form-group row">
 		<label class="col-sm-3 col-form-label" for="eMail">
           Insert Email:</label>
-		<div class="col-sm-9">
+		<div class="col-sm-5">
 			<input class="form-control" name="eMail" id="eMail"
                    type="email" required />
 		</div>
 	</div>
 	<div class="form-group row">      
 		<div class="col-sm-9 offset-sm-3">
-			<button type="submit">Submit</button>
+			<button class="btn btn-primary" style="background-color: #f89ec9; border-color: #f89ec9" type="submit">Submit</button>
 		</div>
 	</div>
 </form>
@@ -31,13 +31,13 @@ if ($result->num_rows > 0) {
 	$row = $result->fetch_array();
 	$qn = $row["PwdQuestion"];
 	echo "<form action='forget2.php' method='post'>";
-	echo "<div class='form-group row'><label class='col-sm-5 col-form-label'>Security Question: $qn</label></div>";
-	echo "<div class='form-group row'><label class='col-sm-3 col-form-label' for='answer'>Security Answer:</label><div class='col-sm-9'>";
+	echo "<div class='form-group row'><label class='col-sm-3 col-form-label'>Security Question:</label><label class='col-sm-5 col-form-label'>$qn</label></div>";
+	echo "<div class='form-group row'><label class='col-sm-3 col-form-label' for='answer'>Security Answer:</label><div class='col-sm-5'>";
 	echo '<input class="form-control" name="answer" id="answer" type="text" required /></div></div>';
-	echo '<div class="form-group row"><button type="submit" class="btn-default">Reset Password</button></div>';
+	echo '<div class="form-group row"><div class="col-sm-3"></div><div><button type="submit" class="btn btn-primary" style="background-color: #f89ec9; border-color: #f89ec9" class="btn-default">Reset Password</button></div></div>';
 }
 else{
-	echo "<script>alert(Invalid Email Address)</script>";
+	echo "<script>alert('Invalid Email Address')</script>";
 }
 ?>
 
